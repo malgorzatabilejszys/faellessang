f87467a8a9d8a3a5641ae092866d33585a475a1c
malgorzatabilejszys <malgorzata.bilejszys@gmail.com>
2025-05-13 22:08:54 +0200

    Update song.ts

--- types/song.ts
+++ types/song.ts
@@ -1,10 +1,10 @@
 export interface Song {
-  id: string
-  number: number
-  title: string
-  author?: string
-  composer?: string
-  lyrics?: string
-  year?: number
-  category?: string
+  id: string;
+  number: number;
+  title: string;
+  lyrics: string;
+  author?: string;  // Optional property (note the ?)
+  composer?: string;  // Optional property
+  year?: number;  // Optional property
+  category?: string;  // Optional property
 }
